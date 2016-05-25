b94ecd64e50baa7529d4e2e3da2bb9f9ec686786
Alex Harvey <Alex.Harvey@team.telstra.com>
2016-05-25 20:45:52 +1000

    Replace net-tools with iproute

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -48,7 +48,6 @@ describe service('nexus') do
   it { is_expected.to be_running }
 end
 
-# TODO: this requires the net-tools package but how to install during test setup?
-#describe port(8082) do
-#  it { is_expected.to be_listening.with('tcp') }
-#end
+describe port(8082) do
+  it { is_expected.to be_listening.with('tcp') }
+end
